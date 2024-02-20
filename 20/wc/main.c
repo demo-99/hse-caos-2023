2b8b35cbe5bc2c1fc8088f618bd435064ada5235
Mikhail Malafeev <m2malafeev@gmail.com>
2024-02-20 19:23:36 +0300

    sem20

--- 20/wc/main.c
+++ 20/wc/main.c
@@ -6,6 +6,38 @@
 #include <unistd.h>
 
 void count_lines(const char* file_path) {
+    int fd = open(file_path, O_RDONLY);
+    if (fd == -1) {
+        printf("Error opening file");
+        return;
+    }
+
+    struct stat fileInfo;
+    if (fstat(fd, &fileInfo) == -1) {
+        printf("fstat failed\n");
+        return;
+    }
+    if (fileInfo.st_size == 0) {
+        printf("%s 0\n", file_path);
+        return;
+    }
+    char* data = mmap(NULL, fileInfo.st_size, PROT_READ, MAP_SHARED, fd, 0);
+
+    int count = 0;
+    for(size_t i = 0; i < fileInfo.st_size; ++i) {
+        if (data[i] == '\n') {
+            ++count;
+        }
+    }
+    if(data[fileInfo.st_size - 1] != '\n') {
+        ++count;
+    }
+    printf("%s %d\n", file_path, count);
+
+    munmap(data, fileInfo.st_size);
+    close(fd);
+
+    return;
 }
 
 int main(int argc, char* argv[]) {
