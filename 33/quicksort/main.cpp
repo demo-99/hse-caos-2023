19ef766d65d9b77b88571e5bc232aeb41c0d739d
Mikhail Malafeev <m2malafeev@gmail.com>
2024-06-11 18:05:37 +0300

    sem33

--- 33/quicksort/main.cpp
+++ 33/quicksort/main.cpp
@@ -3,20 +3,6 @@
 #include <thread>
 #include <future>
 
-int partition(std::vector<int>& arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j <= high - 1; ++j) {
-        if (arr[j] < pivot) {
-            ++i;
-            std::swap(arr[i], arr[j]);
-        }
-    }
-    std::swap(arr[i + 1], arr[high]);
-    return i + 1;
-}
-
 void multithreaded_quicksort(std::vector<int>& arr, int low, int high) {
     // TODO
 }
